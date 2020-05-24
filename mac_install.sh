e3b1a8728c25366690438b76394fb141cd7881a4
jiqimaogou <fjjohnchen@gmail.com>
2020-05-24 11:02:46 +0800

    Update mac_install.sh

--- mac_install.sh
+++ mac_install.sh
@@ -119,6 +119,7 @@ export PATH=$PATH:$JAVA_HOME/bin
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/emulator       # can't run emulator without it
 export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 
 export ANDROID_SDK_HOME=$HOME/Library/Android/sdk
