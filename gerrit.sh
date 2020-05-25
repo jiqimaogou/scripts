1e5c9e0b147d2522958c3dfffc51af5d500e6932
jiqimaogou <fjjohnchen@gmail.com>
2020-05-25 23:42:23 +0800

    Update gerrit.sh

--- gerrit.sh
+++ gerrit.sh
@@ -3,7 +3,7 @@ brew install nginx
 
 wget -c https://gerrit-releases.storage.googleapis.com/gerrit-3.1.4.war
 
-java -jar gerrit.war init --batch -d review_site
+java -jar gerrit-3.1.4.war init --batch -d review_site
 
 sh review_site/bin/gerrit.sh start
 
@@ -13,3 +13,4 @@ cd review_site/etc
 
 touch passwd
 
+htpasswd -b passwd admin admin
