002610894ae76991ca31a917c2b4a1a666d8d883
jiqimaogou <fjjohnchen@gmail.com>
2020-05-26 00:06:46 +0800

    Update gerrit.sh

--- gerrit.sh
+++ gerrit.sh
@@ -20,3 +20,10 @@ cd -
 sh review_site/bin/gerrit.sh start
 
 open http://localhost:8081
+
+cp /usr/local/etc/nginx/nginx.conf review_site
+cp /usr/local/etc/nginx/mime.types review_site
+
+nginx -c review_site/nginx/conf
+
+nginx -s reload
